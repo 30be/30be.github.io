97d0e0fdd1c2e276cdef5e5d922335fa264e863b
Lykd <lykd@pm.me>
2025-10-05 03:44:57 +0300

    Added my site here for testing speeds

---
+++ _site/search.js
@@ -0,0 +1,40 @@
+fetch('/search.json')
+  .then(response => response.json())
+  .then(posts => {
+    const input = document.getElementById('search-input');
+    const resultsContainer = document.getElementById('search-results')
+    const searchPosts = posts.map(post => ({
+      ...post,
+      searchTitle: post.postTitle.toLowerCase(),
+      postContent: post.postContent.toLowerCase(),
+    }));
+    const updateResults = (e) => {
+      query = e.target.value.toLowerCase().trim()
+      resultsContainer.innerHTML = '';
+      if (query.length < 2) return;
+
+      const filtered = searchPosts.filter(post =>
+        post.searchTitle.includes(query) ||
+        post.postContent.includes(query)
+      );
+
+      const p = document.createElement('p');
+      p.textContent = `${filtered.length} results found`
+      resultsContainer.appendChild(p);
+
+      if (filtered.length === 0) return;
+
+      const results = document.createElement('ul');
+      resultsContainer.appendChild(results);
+
+      filtered.forEach(post => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = `/${post.postRoute}`;
+        a.textContent = `${post.postTitle} (${post.postDate})`;
+        li.appendChild(a);
+        results.appendChild(li);
+      });
+    };
+    input.addEventListener('input', updateResults)
+  });
